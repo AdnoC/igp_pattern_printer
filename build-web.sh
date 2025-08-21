8b1fae8c8aef01471741156f53c81af83c4d71aa
AdnoC <adam.r.cutler@gmail.com>
2025-08-21 15:05:19 -0400

    update build

--- build-web.sh
+++ build-web.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+git branch -D web
+git co -b web
+
 cd wasm
 trunk build -d ../docs --release
 echo 'igp-pattern-printer.adno.page' > ../docs/CNAME
+git add ../docs
+git ci -am 'build'
+git push -f
